cf9521a6a159f087d5d27a3dbd2b3df6c70d53a9
iamkulikov <i.am.kulikov@gmail.com>
2023-02-16 10:51:55 +0300

    + better model list incl NIIP ex GGCB

--- download_script/do_import.R
+++ download_script/do_import.R
@@ -3,8 +3,8 @@ library(here)
 
 ##### Choose import mode
 test <- 0
-update_mode <- 0    # 0 if all the data and containers should be new, 1 if only update
-n_attempts <- 10     # how many times should we ping API's for needed data
+update_mode <- 1    # 0 if all the data and containers should be new, 1 if only update
+n_attempts <- 5     # how many times should we ping API's for needed data
 formula_words <- c("lag", "rollsum", "rollavg", "rollvol", "mean", "last", "first", "min", "max", "sum", "coalesce", "share", "exp")
 
 ##### Where is import schedule saved? What are the data files?

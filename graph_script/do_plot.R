19bf1b4c76f22c5f82e1cd0950a1c86838cdc5ae
iamkulikov <i.am.kulikov@gmail.com>
2023-07-23 16:50:58 +0300

    + import data and peers for function version of plot

--- graph_script/do_plot.R
+++ graph_script/do_plot.R
@@ -7,11 +7,12 @@ here::i_am("_country_analysis_scripts/graph_script/do_plot.R")
 ##### Parameters and source names
 country_name <- "China"
 file_output <- "jpg"
-horizontal_size <- c()
-vertical_size <- c()
-data_fname <- "Filled_DB.xlsx"
-peers_fname <- "1_peers_params.xlsx"
-plotparam_fname <- "2_graphlib.xlsx"
+horizontal_size <- c(1800, 900)
+vertical_size <- c(900, 900)
+data_fname <- here("_DB", "Filled_DB.xlsx")
+data_d_fname <- here("_DB", "Filled_d_DB.xlsx")
+peers_fname <- here("_DB", "1_peers_params.xlsx")
+plotparam_fname <- here(country_name, "Auto_report", "2_graphlib.xlsx")
 
 ##### Import function definitions
 source(here("_country_analysis_scripts","download_script","import.R"))
@@ -19,15 +20,16 @@ source(here("_country_analysis_scripts","download_script","fill.R"))
 source(here("_country_analysis_scripts","graph_script","plot.R"))
 
 ###### Import data from local database
-D <- importFilledData(data_fname = here("_DB", data_fname))
+D <- importFilledData(data_fname = data_fname, data_d_fname = data_d_fname)
 
 ###### Determining country focus and peers
-country_iso2c <- unique(extdata_y$country_id[extdata_y$country == country_name])
-
+country_info <- getPeersCodes(country_name = country_name, peers_fname = peers_fname)
 
 ##### Import plotting schedule
+graphdata <- getPlotSchedule(plotparam_fname = plotparam_fname)
 
 ##### Generating sources
+graphdata <- generatePlotSources(graphdata = graphdata, dict = dict)
 
 ##### Plotting cycle
   
@@ -40,4 +42,4 @@ country_iso2c <- unique(extdata_y$country_id[extdata_y$country == country_name])
   ### Produce graph
 
 
-
+##### Saving
--- graph_script/plot.R
+++ graph_script/plot.R
@@ -27,9 +27,9 @@ ACRA <- newpal( col = c(rgb(147, 202, 116, maxColorValue = 255),rgb(153, 38, 115
 #using +scale_color_manual(values = ACRA)
 
 
-####### Import filled data
+####### Function to import filled data
 
-importFilledData <- function(data_fname) {
+importFilledData <- function(data_fname, data_d_fname) {
   
   for (i in c("y", "q", "m")) {
     eval(parse(text = glue("ncols <- length(read_excel(data_fname, sheet = '{i}', col_names = T, skip=0, n_max = 0))") ))
@@ -37,8 +37,8 @@ importFilledData <- function(data_fname) {
                             col_types = c('text', 'text', rep('numeric', ncols-2)))") ))
   }
   
-  ncols <- length(read_excel(data_fname, sheet = "d", col_names = T, skip=0, n_max = 0))
-  extdata_d <- read_excel(data_fname, sheet = "d", col_names = T, skip=0,
+  ncols <- length(read_excel(data_d_fname, sheet = "d", col_names = T, skip=0, n_max = 0))
+  extdata_d <- read_excel(data_d_fname, sheet = "d", col_names = T, skip=0,
                           col_types = c("text", "text", "date", rep("numeric", ncols-3)))
   
   extdata_y <- extdata_y %>% mutate(time=year-1987)
@@ -49,10 +49,53 @@ importFilledData <- function(data_fname) {
   ncols <- length(read_excel(data_fname, sheet = "dict", col_names = T, skip=0, n_max = 0))
   dict <- read_excel(data_fname, sheet = "dict", col_names = T, skip=0,
                      col_types = rep("text", ncols))
+  dict_d <- read_excel(data_d_fname, sheet = "dict_d", col_names = T, skip=0,
+                     col_types = rep("text", ncols))
+  dict <- rbind(dict, dict_d)
   
   return(list(extdata_y = extdata_y, extdata_q = extdata_q, extdata_m = extdata_m, extdata_d = extdata_d, dict = dict))
   
 }
 
 
-####### Import filled data
+####### Function to get country's codes and the codes of its peers
+
+getPeersCodes <- function(country_name, peers_fname) {
+  
+  groupsdata <- t(read_excel(peers_fname, sheet = "groups", col_names = T, n_max = 11))
+  labels <- c("region", groupsdata[3,-1])
+  groupsdata <- data.frame(groupsdata[-c(1:3),])
+  colnames(groupsdata) <- labels
+  country_iso3c <- groupsdata %>% filter(rownames(groupsdata) == country_name) %>% pull(country_code)
+  country_iso2c <- countrycode(country_iso3c, origin = 'iso3c', destination = 'iso2c')
+  groupsdata <- groupsdata %>% select(-c(region)) %>% 
+      mutate(country_iso2c = countrycode(country_code, origin = 'iso3c', destination = 'iso2c')) %>%
+      rename('country_iso3c' = 'country_code') %>% as_tibble
+  
+  peersdata <- read_excel(peers_fname, sheet = "groups", col_names = T, skip=11)
+  peers_default_iso3c <- names(peersdata)[peersdata[peersdata$country_code == country_iso3c, ] == 1]
+  peers_default_iso2c <- countrycode(peers_default_iso3c, origin = 'iso3c', destination = 'iso2c')
+  peers_neighbours_iso3c <- peersdata %>% select(region, country_code) %>% filter(region==peersdata$region[peersdata$country_code==country_iso3c]) %>%
+    pull(country_code)
+  peers_neighbours_iso2c <- countrycode(peers_neighbours_iso3c, origin = 'iso3c', destination = 'iso2c')
+  
+  return(list(country_iso2c = country_iso2c, country_iso3c = country_iso3c, peers_default_iso2c = peers_default_iso2c, 
+              peers_default_iso3c = peers_default_iso3c, peers_neighbours_iso2c = peers_neighbours_iso2c, 
+              peers_neighbours_iso3c = peers_neighbours_iso3c, regions = groupsdata))
+}
+
+
+####### Function to get plot schedule
+
+getPlotSchedule <- function(plotparam_fname) {
+  
+  
+}
+
+####### Function to generate plot sources
+
+generatePlotSources <- function(graphdata, dict) {
+  
+  
+}
+

ab505966f43e9c34d0602557f51bb4366088296a
iamkulikov <i.am.kulikov@gmail.com>
2023-07-26 15:25:12 +0300

    + parseGraphPlan

--- graph_script/do_plot.R
+++ graph_script/do_plot.R
@@ -22,31 +22,49 @@ source(here("_country_analysis_scripts","download_script","import.R"))
 source(here("_country_analysis_scripts","download_script","fill.R"))
 source(here("_country_analysis_scripts","graph_script","plot.R"))
 
-###### Import data from local database
+###### Import data from the local database
 D <- importFilledData(data_fname = data_fname, data_d_fname = data_d_fname)
 
 ###### Determining country focus and peers
 country_info <- getPeersCodes(country_name = country_name, peers_fname = peers_fname)
 
-##### Import plotting schedule and generate sources
+##### Importing plotting schedule and generating sources
 graphplan <- getPlotSchedule(plotparam_fname = plotparam_fname, dict = D$dict)
 
 ##### Check integrity of the plans
 graphplan <- checkGraphTypes(graphplan = graphplan, graph_types = graph_types)
 graphplan <- checkUnique(graphplan = graphplan)
-graphplan <- checkAvailability(graphplan = graphplan, dict = D$dict) %>% mutate(checks = check_types*check_unique*check_availability)
+graphplan <- checkPeers(graphplan = graphplan, peer_groups = country_info$regions)
+graphplan <- checkAvailability(graphplan = graphplan, dict = D$dict) %>% mutate(checks = check_types*check_unique*check_peers*check_availability)
 error_report <- graphplan %>% filter(checks == 0, active == 1)
 #error_report
 
-##### Plotting cycle
+if (is.null(dim(error_report)[1]) | is.na(dim(error_report)[1]) | (dim(error_report)[1] == 0)) {
   
-  ### Parse graph parameters  
+  print("Checks passed")
 
-  ### Filter data
+    ##### Plotting cycle for each row of the plan
+    for(i in seq_along(graphplan$graph_name)) {    
+  
+      ### Parsing single graph parameters  
+      graph_params <- parseGraphPlan(graphrow = graphplan[i,], dict = D$dict, horizontal_size = horizontal_size, vertical_size = vertical_size)
+      
+      ### Fixing peers
+      peers_iso2c <- fixPeers(country_info = country_info, peers = graph_params$peers)
+      
+      ### Filtering data
+      S <- subsetData(D, country_iso2c, peers_iso2c)
+      
+      ### Using needed graph type 
+      
+      
+    
+      ### Producing graph
+    
+    
+    ##### Saving file
 
-  ### Use needed graph type 
-
-  ### Produce graph
-
-
-##### Saving
+      
+    }
+  
+} else {print("Errors found"); print(error_report)}
